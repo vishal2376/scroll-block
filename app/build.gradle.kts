537cb34146d367758266eecce2d16f4e11a80dfd
Vishal Singh <vishalsingh2376@gmail.com>
2024-06-25 17:04:53 +0530

    chore: disabled dependency metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,13 @@ android {
 			"$projectDir/schemas"
 		)
 	}
+	dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+
 }
 
 dependencies {
