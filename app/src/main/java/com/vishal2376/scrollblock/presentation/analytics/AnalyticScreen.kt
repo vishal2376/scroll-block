26fd1ad7c73c96b7493ffbdc6e440e3c847748d6
vishal2376 <vishalsingh2376@gmail.com>
2024-07-01 22:53:35 +0530

    feat(analytics): add temp pie chart

--- app/src/main/java/com/vishal2376/scrollblock/presentation/analytics/AnalyticScreen.kt
+++ app/src/main/java/com/vishal2376/scrollblock/presentation/analytics/AnalyticScreen.kt
@@ -1,14 +1,21 @@
 package com.vishal2376.scrollblock.presentation.analytics
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -28,14 +35,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vishal2376.scrollblock.R
+import com.vishal2376.scrollblock.domain.model.TimeWastedInfo
 import com.vishal2376.scrollblock.presentation.analytics.components.SummaryItemComponent
+import com.vishal2376.scrollblock.presentation.common.CustomPieChart
+import com.vishal2376.scrollblock.presentation.common.descriptionStyle
+import com.vishal2376.scrollblock.presentation.common.fontMontserrat
 import com.vishal2376.scrollblock.presentation.common.h1style
 import com.vishal2376.scrollblock.presentation.common.h2style
+import com.vishal2376.scrollblock.presentation.home.components.PieChartIndicatorComponent
 import com.vishal2376.scrollblock.ui.theme.ScrollBlockTheme
+import com.vishal2376.scrollblock.ui.theme.pieChartColors
 import com.vishal2376.scrollblock.ui.theme.white
+import com.vishal2376.scrollblock.utils.formatTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -78,11 +94,72 @@ fun AnalyticScreen(onBack: () -> Unit) {
 				Modifier
 					.fillMaxWidth()
 					.height(350.dp)
-					.padding(top = 24.dp)
 					.clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
 					.background(blackGradient),
 				horizontalAlignment = Alignment.CenterHorizontally,
-			) { }
+			) {
+				val timeWastedList = listOf(
+					TimeWastedInfo("Instagram", 30000),
+					TimeWastedInfo("Youtube", 13000),
+					TimeWastedInfo("Linkedin", 5000),
+					TimeWastedInfo("Snapchat", 1200)
+				)
+
+				val totalTimeWasted =
+					timeWastedList.filter { it.timeWasted > 0 }.map { it.timeWasted }
+
+
+				// pie chart
+				Box(
+					modifier = Modifier
+						.clip(CircleShape)
+						.clickable {
+//							onNavigate(Screen.AnalyticScreen.name)
+						},
+					contentAlignment = Alignment.Center
+				) {
+					Column(
+						modifier = Modifier.padding(top = 8.dp),
+						horizontalAlignment = Alignment.CenterHorizontally
+					) {
+						Text(
+							text = "This Week",
+							textAlign = TextAlign.Center,
+							style = descriptionStyle
+						)
+						Text(
+							text = formatTime(totalTimeWasted.sum()),
+							textAlign = TextAlign.Center,
+							fontSize = 24.sp,
+							fontFamily = fontMontserrat,
+						)
+					}
+					CustomPieChart(
+						data = totalTimeWasted, pieChartSize = 190.dp
+					)
+				}
+
+				Spacer(modifier = Modifier.height(16.dp))
+
+				// pie chart indicator
+				LazyVerticalGrid(
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(horizontal = 32.dp),
+					columns = GridCells.Fixed(2),
+					verticalArrangement = Arrangement.spacedBy(8.dp),
+					horizontalArrangement = Arrangement.spacedBy(24.dp),
+				) {
+					items(timeWastedList) {
+						PieChartIndicatorComponent(
+							appName = it.name,
+							time = it.timeWasted,
+							color = pieChartColors[timeWastedList.indexOf(it) % pieChartColors.size]
+						)
+					}
+				}
+
+			}
 
 			Row(
 				modifier = Modifier.padding(horizontal = 16.dp),
@@ -106,17 +183,20 @@ fun AnalyticScreen(onBack: () -> Unit) {
 					horizontalArrangement = Arrangement.spacedBy(
 						8.dp,
 						Alignment.CenterHorizontally
-					)
+					),
+					verticalAlignment = Alignment.CenterVertically
 				) {
 					SummaryItemComponent(
 						title = "Time\nWasted",
-						info = "21h 23m",
-						icon = R.drawable.clock
+						info = "13h 40m",
+						icon = R.drawable.clock,
+						index = 1
 					)
 					SummaryItemComponent(
 						title = "Total\nScrolls",
 						info = "12.3k",
-						icon = R.drawable.scroll
+						icon = R.drawable.scroll,
+						index = 2
 					)
 				}
 				Row(
@@ -124,17 +204,20 @@ fun AnalyticScreen(onBack: () -> Unit) {
 					horizontalArrangement = Arrangement.spacedBy(
 						8.dp,
 						Alignment.CenterHorizontally
-					)
+					),
+					verticalAlignment = Alignment.CenterVertically
 				) {
 					SummaryItemComponent(
 						title = "Scroll\nBlocked",
 						info = "3.1k",
-						icon = R.drawable.shield
+						icon = R.drawable.shield,
+						index = 3
 					)
 					SummaryItemComponent(
 						title = "App\nOpened",
 						info = "1.8k",
-						icon = R.drawable.dashboard
+						icon = R.drawable.dashboard,
+						index = 4
 					)
 				}
 			}
